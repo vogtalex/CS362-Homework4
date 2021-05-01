a5eab5192bf7eaf54146cbd1f0a64941363358e3
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-01 13:57:35 -0700

    Created function that concatenates a first and last name into a full name

---
+++ fullname.py
@@ -0,0 +1,6 @@
+def createFullName(firstName, lastName):
+	space = ' '
+	fullname = firstName + space + lastName
+	return (fullname)
+
+print('\nThis program will output a full name when given a first and last name')
