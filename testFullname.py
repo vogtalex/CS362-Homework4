329184dcd26827907d2f9ebafa2127d638633022
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-01 14:04:43 -0700

    Added two more unit tests for numbers and miscellaneous characters

--- testFullname.py
+++ testFullname.py
@@ -6,6 +6,14 @@ class testCaseAdd(unittest.TestCase):
 		self.assertEqual(fullname.createFullName('Alex', "Vogt"), 'Alex Vogt')
 		self.assertEqual(fullname.createFullName('Jeff', "Bezos"), 'Jeff Bezos')
 
+	def test_add_numbers(self):
+		self.assertEqual(fullname.createFullName('1', "2"), '1 2')
+		self.assertEqual(fullname.createFullName('25', "89"), '25 89')
+
+	def test_add_miscellaneous_characters(self):
+		self.assertEqual(fullname.createFullName('$teelers f@n', "Black-Yellow"), '$teelers f@n Black-Yellow')
+		self.assertEqual(fullname.createFullName('*&^%', "<>?:"), '*&^% <>?:')
+
 
 if __name__ == '__main__':
 	unittest.main()
