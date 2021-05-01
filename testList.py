e28cc914513e25aefe3508dc1cbb42bbb12bcc5f
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-01 13:48:56 -0700

    Added two more unit tests for empty lists and negative numbers

--- testList.py
+++ testList.py
@@ -8,7 +8,15 @@ class testCaseAdd(unittest.TestCase):
 		self.assertEqual(list.listAverage(list1), 6)
 		self.assertEqual(list.listAverage(list2), 12.25)
 
-	
+	def test_add_empty(self):
+		list3 = []
+		self.assertEqual(list.listAverage(list3), 0)
+
+	def test_add_negatives(self):
+		list4 = [-2, -4, -6, -8, -10]
+		list5 = [-1, 13 ,-22, 13]
+		self.assertEqual(list.listAverage(list4), -6)
+		self.assertEqual(list.listAverage(list5), 0.75)
 
 if __name__ == '__main__':
 	unittest.main()
