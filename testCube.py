859cdd8625220ce1d9fde4421a29925e888d83ec
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-01 13:30:01 -0700

    Added two more unit tests for decimals and negative numbers

--- testCube.py
+++ testCube.py
@@ -6,8 +6,13 @@ class testCaseAdd(unittest.TestCase):
 		self.assertEqual(cube.cubeVolume(10), 1000)
 		self.assertEqual(cube.cubeVolume(3), 27)
 
+	def test_add_negative(self):
+		self.assertEqual(cube.cubeVolume(-10), -1000)
+		self.assertEqual(cube.cubeVolume(-3), -27)
 
-
+	def test_add_decimals(self):
+		self.assertEqual(cube.cubeVolume(10.5), 1157.625)
+		self.assertEqual(cube.cubeVolume(3.4), 39.304)
 
 if __name__ == '__main__':
 	unittest.main()
