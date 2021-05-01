fe4197f9c91bc50fce7da4ef3bbb323fbdc2eb15
Alex Vogt <vogtalex@oregonstate.edu>
2021-05-01 13:40:25 -0700

    Created function that calculates average of a given list

---
+++ list.py
@@ -0,0 +1,8 @@
+def listAverage(list):
+	if len(list) == 0:
+		return 0
+
+	average = sum(list) / len(list)
+	return (average)
+
+print('\nThis program will output the average of a list of numbers.')
